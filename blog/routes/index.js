eca047419eb3287f29fdbded8b0d0bd8a1e52e1b
liukunpeng <liukunpeng@foundao.com>
2016-03-10 16:24:40 +0800

    增加留言功能

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -2,6 +2,7 @@
 var crypto = require('crypto');
 var User = require("../models/user.js");
 var Post = require("../models/post.js");
+var Comment = require("../models/comment.js")
 
 module.exports = function(app){
 	app.get('/',function(req,res){
@@ -228,6 +229,29 @@ module.exports = function(app){
 			res.redirect('/');
 		});
 	});
+
+	app.post("/u/:name/:day/:title",function(req,res){
+		var date = new Date(),
+			time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + (date.getMinutes() < 10 ? '0'+date.getMinutes():date.getMinutes());
+
+		var comment = {
+			name: req.body.name,
+			email: req.body.email,
+			website: req.body.website,
+			time: time,
+			content: req.body.content
+		};
+
+		var newComment = new Comment(req.params.name,req.params.day,req.params.title,comment);
+		newComment.save(function(err){
+			if(err){
+				req.flash('error',err);
+				return res.redirect('back');
+			}
+			req.flash('success',"留言成功");
+			res.redirect('back');
+		});
+	})
 	function checkLogin(req,res,next){
 		if(!req.session.user){
 			req.flash('error','未登录');
